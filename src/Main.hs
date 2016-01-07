02aa9c75fd66841a1e5705e43cf2c645b4bede68
Kevin Li <kevin.li195@hotmail.com>
2016-01-07 02:24:15 -0500

    Adds some work on saving the state of allowalert.

--- src/Main.hs
+++ src/Main.hs
@@ -7,7 +7,19 @@ import System.Posix.Process
 -- the desire timeout of the message.
 currentPath = "/sys/class/power_supply/BATC/charge_now"
 fullPath = "/sys/class/power_supply/BATC/charge_full"
-alertTimeout = 1
+allowAlertPath = "$HOME/.batterymonitorinfo"
+alertThreshold = 0.15 :: Float
+alertTimeout = 5
+
+-- Called when alert appears to prevent alert from appearing again if charge is still below threshold.
+writeAllowAlert :: Bool -> IO ()
+writeAllowAlert
+    | True = writeFile alertPath "1"
+    | False = writeFile alertPath "0"
+
+-- Checks the current threshold file setting to determine whether an alert should be instantiated.
+readAllowAlert :: IO Bool
+readAllowAlert = readFile alertPath >>= readIO
 
 -- Produces message given a percentage of battery remaining.
 message :: Int -> String
@@ -26,15 +38,29 @@ alert m = executeFile "zenity" True
                 Nothing
 
 -- Use this function for battery messages because boilerplate.
+-- This function will alert at any level and then set allowAlert to False.
 batteryAlert :: Float -> IO ()
-batteryAlert a
-    | a < 1.0 = alert $ message $ floor $ a * 100
-    | otherwise = return ()
+batteryAlert a = do
+    alert $ message $ floor $ a * 100
+    writeAllowAlert False
 
 main = do
     c <- readFile currentPath
     f <- readFile fullPath
     let a = read $ unwords $ words c :: Float
     let b = read $ unwords $ words f :: Float
+    -- d is the current percentage charge. It is of type Float due to the type casting above.
     let d = a / b
-    batteryAlert d
+    t <- readAllowAlert
+    case t of
+            True -> 
+                -- If we do work LEFT OFF HERE CONTINUE HERE NOW
+                if d < alertThreshold then do
+                    batteryAlert d
+                else
+                    return ()
+            False ->
+                -- If we do not alert, then we check if we are above the alert threshold.
+                if d > alertThreshold then writeAllowAlert True
+            _ ->
+                writeAllowAlert True
