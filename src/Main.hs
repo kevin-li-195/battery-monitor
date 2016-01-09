fe7a43b31ea5ab0c66537cb0d5bc9b167b111b22
Kevin Li <kevin.li195@hotmail.com>
2016-01-09 18:38:18 -0500

    Implements daemonizing and command line for desired alert threshold.

--- src/Main.hs
+++ src/Main.hs
@@ -3,8 +3,10 @@ import Control.Exception
 import Control.Monad
 import Data.Char
 import System.Environment
+import System.Exit
 import System.IO.Error
 import System.Posix.Files
+import System.Posix.Daemonize
 import System.Posix.Types
 import System.Posix.Process
 
@@ -15,8 +17,8 @@ currentPath = "/sys/class/power_supply/BATC/charge_now" :: FilePath
 fullPath = "/sys/class/power_supply/BATC/charge_full" :: FilePath
 -- allowAlertPath provides the $HOME/.battery-monitorinfo file path.
 allowAlertPath = (++) <$> getEnv "HOME" <*> return "/.battery-monitorinfo" :: IO String
-alertThreshold = 0.1 :: Float
-alertTimeout = 5 :: Int
+defaultAlertThreshold = 0.1 :: Float
+-- alertTimeout = 5 :: Int
 
 -- | Called when alert appears to prevent alert from appearing again if charge is still below threshold.
 writeAllowAlert :: Bool -> IO ()
@@ -45,11 +47,10 @@ message a = "Battery:\n\n" ++ show a ++ "% remaining."
 -- | Makes a alert using Zenity according to the string given.
 alert :: String -> IO ()
 alert m = executeFile "zenity" True
-                [ "--timeout"
-                , show alertTimeout
-                , "--warning"
+                [ "--warning"
                 , "--text"
-                , "--display=:0.0"
+                -- , "--timeout"
+                -- , show alertTimeout
                 , m
                 ]
                 Nothing
@@ -62,7 +63,25 @@ batteryAlert a = do
     forkProcess $ alert . message . floor $ a * 100
     return ()
 
-main = forever $ do
+formatPercent :: Float -> String
+formatPercent = show . floor . (*100)
+
+usage :: IO ()
+usage = do
+    putStrLn "\nUsage of battery-monitor: battery-monitor [threshold]\n"
+    putStrLn "Running battery-monitor without a threshold will default to a 10% capacity alert threshold.\n"
+    putStrLn "You should provide a number less than 1.0 too.\n"
+    putStrLn "Example usage: battery-monitor 0.2"
+    putStrLn "The above example will result in the battery monitor prompting an alert when the capacity falls below 20%.\n"
+    putStrLn "Example usage: battery-monitor"
+    putStrLn "The above example will result in the battery monitor prompting an alert when the capacity falls below 10%.\n"
+    putStrLn "The .battery-monitorinfo file is used to preserve whether or not alerts are allowed so that you're not constantly alerted when capacity falls below the threshold.\n"
+    putStrLn "WARNING: Running two instances of battery-monitor will result in repeated alerts.\n"
+    exitFailure
+
+-- | Core daemon running function.
+daemon :: Float -> IO ()
+daemon threshold = forever $ do
     threadDelay 60000000
     c <- readFile currentPath
     f <- readFile fullPath
@@ -76,7 +95,7 @@ main = forever $ do
         True -> 
             -- If we allow an alert then we check if the current percentage
             -- charge is below the alert threshold.
-            if d < alertThreshold then do
+            if d < threshold then do
                 batteryAlert d
             else do
                 return ()
@@ -84,5 +103,23 @@ main = forever $ do
             -- If we do not alert, then we check if we are above the alert threshold.
             -- That way, we'll be able to re-allow alerts when we go back above the
             -- alert threshold.
-            if d > alertThreshold then writeAllowAlert True
+            if d > threshold then writeAllowAlert True
             else return ()
+
+handler :: [Float] -> IO ()
+handler [] = do
+    putStrLn "Running battery-monitor daemon with 10% alert threshold."
+    daemonize $ daemon defaultAlertThreshold
+handler [a] = do
+    if a < 1.0 then do
+        putStrLn $ "Running battery-monitor daemon with " ++ (formatPercent a) ++ "% alert threshold."
+        daemonize $ daemon a
+    else
+        usage
+handler (x:xs) = usage
+
+main = do
+    arg <- tryIOError $ getArgs >>= (mapM readIO)
+    case arg of
+        Left a -> usage
+        Right a -> handler a
